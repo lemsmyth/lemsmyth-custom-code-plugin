2dc630c4e69c0369848f30c4f606724489fee228
Lem <lemsmyth@yahoo.com>
2020-07-06 18:42:42 -0500

    initial commit

---
+++ site-specific-code.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Site Specific Code
+ * Plugin URI: https://lemsmyth.com/plugins/site-specific-code/
+ * Description: Add custom website specific code that adds functionality here.
+ * Version: 0.1.0
+ * Author: lemsmyth
+ * Author URI: https://lemsmyth.com/
+ * Donate Link: https://lemsmyth.com/donate/
+ * Requires PHP: 5.6.20
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: ssc
+ * Domain Path: /languages
+ */
+if ( !defined( 'ABSPATH' ) ) exit;
