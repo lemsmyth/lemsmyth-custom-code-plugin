ad2673cea753a5c95a57c57d128e4bbd7d3dd805
Lem <lemsmyth@yahoo.com>
2020-07-20 13:52:25 -0500

    Basic Structure

--- site-specific-code.php
+++ site-specific-code.php
@@ -2,15 +2,15 @@
 /**
  * Plugin Name: ❤︎❤︎ lemsmyth.com Custom Code
  * Plugin URI: https://lemsmyth.com/plugins/site-specific-custom-code/
- * Description: Add custom site specific code here.
+ * Description: Add custom site specific code here (i.e. code that is not theme related and you wish to have available even if you change your theme should go in a plugin).
  * Version: 0.1.0
  * Author: lemsmyth
  * Author URI: https://lemsmyth.com/
  * Donate Link: https://lemsmyth.com/donate/
  * Requires PHP: 5.6.20
- * License: GPL2 or later
+ * License: GNU General Public License v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: lems
  * Domain Path: /languages
  */
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( !defined( 'ABSPATH' ) ) exit; // If accessed directly die!
